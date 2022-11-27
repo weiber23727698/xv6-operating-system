af99b9970b53832978370e500d6c147aee1538d4
weiber23727698 <72683941+weiber23727698@users.noreply.github.com>
2022-11-27 13:40:14 +0800

    Update threads.h

--- MP1/user/threads.h
+++ MP1/user/threads.h
@@ -1,10 +1,7 @@
 #ifndef THREADS_H_
 #define THREADS_H_
 
-// TODO: necessary includes, if any
 #include "user/setjmp.h"
-// TODO: necessary defines, if any
-
 
 struct thread {
     void (*fp)(void *arg);
@@ -13,7 +10,7 @@ struct thread {
     void *stack_p;
     jmp_buf env;
     int ID;
-    int buf_set; //1: indicate jmp_buf has been set, 0: indicate jmp_buf not set
+    int buf_set; // 1: indicate jmp_buf has been set, 0: indicate jmp_buf not set
     struct thread *left;
     struct thread *right;
     struct thread *parent;
